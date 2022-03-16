b2f6e8301edc74cb7c72a9a41a1076cd95fe2ac3
Milad <millad.99.awad@gmail.com>
2022-03-16 23:42:20 +0200

    Edit subject routes

--- routes/subject_route.js
+++ routes/subject_route.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const SubjectController = require('../controllers/subject_controller')
-const auth = require('../middleware/auth')
-const isAdmin = require('../middleware/is_admin')
+const express = require('express');
+const SubjectController = require('../controllers/subject_controller');
+const auth = require('../middleware/auth');
+const isAdmin = require('../middleware/is_admin');
 
-const router = new express.Router()
+const router = new express.Router();
 
-router.post('/subjects', auth, isAdmin, SubjectController.createSubject)
+router.post('/subjects', auth, isAdmin, SubjectController.createSubject);
 
-router.get('/subjects', auth, isAdmin, SubjectController.getAllSubjects)
+router.get('/subjects', SubjectController.getAllSubjects);
 
-router.get('/subjects/:id', auth, isAdmin, SubjectController.getSubject)
+router.get('/subjects/:id', SubjectController.getSubject);
 
-router.patch('/subjects/:id', auth, isAdmin, SubjectController.updateSubject)
+router.patch('/subjects/:id', auth, isAdmin, SubjectController.updateSubject);
 
-router.delete('/subjects/:id', auth, isAdmin, SubjectController.deleteSubject)
+router.delete('/subjects/:id', auth, isAdmin, SubjectController.deleteSubject);
 
-module.exports = router
+module.exports = router;
